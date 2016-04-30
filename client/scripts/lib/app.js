67c4f778d32444b45bbfbf1d22c81ca3380b53a7
Todd Metheny <todd.metheny@gmail.com>
2016-04-30 18:57:54 -0400

    basic nav for the chat app

---
+++ client/scripts/lib/app.js
@@ -0,0 +1,32 @@
+// Libs
+import angular from 'angular';
+import 'angular-animate';
+import 'angular-meteor';
+import 'angular-sanitize';
+import 'angular-ui-router';
+import 'ionic-scripts';
+ 
+// Modules
+import Definer from '../definer';
+import RoutesConfig from '../routes';
+
+// App
+const App = angular.module('Whatsapp', [
+  'angular-meteor',
+  'ionic'
+]);
+
+new Definer(App)
+  .define(RoutesConfig); 
+ 
+// Startup
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+}
+else {
+  angular.element(document).ready(onReady);
+}
+ 
+function onReady() {
+  angular.bootstrap(document, ['Whatsapp']);
+}
